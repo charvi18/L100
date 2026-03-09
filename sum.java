ce9b4429c3722b42af7f44cd757977c59a6f1774
charvi <charvimanghnani@gmail.com>
2026-03-09 21:00:42 +0530

    Added Java program for sum of two numbers

---
+++ sum.java
@@ -0,0 +1,11 @@
+public class sum{
+    public static void main(String[]args){
+        int a=1;
+        int b=9;
+       int  sum=a+b;
+       System.out.println(sum);
+         
+    }
+    
+
+}
